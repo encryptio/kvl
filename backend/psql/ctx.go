c6b830b932df9047e7d32313a983c8f642298893
Chris Kastorff <encryptio@gmail.com>
2015-02-08 16:09:56 -0800

    backend/psql: added

---
+++ backend/psql/ctx.go
@@ -0,0 +1,122 @@
+package psql
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/lib/pq"
+
+	"git.encryptio.com/kvl"
+)
+
+type ctx struct {
+	sqlTx      *sql.Tx
+	needsRetry bool
+}
+
+func (c *ctx) checkErr(err error) {
+	if pgErr, ok := err.(*pq.Error); ok && pgErr.Code == "40001" {
+		// concurrent update, abort transaction and retry
+		c.needsRetry = true
+	}
+}
+
+func (c *ctx) Get(key []byte) (kvl.Pair, error) {
+	var p kvl.Pair
+
+	row := c.sqlTx.QueryRow("SELECT key, value FROM data WHERE key = $1", key)
+	err := row.Scan(&p.Key, &p.Value)
+	if err != nil {
+		c.checkErr(err)
+		if err == sql.ErrNoRows {
+			return p, kvl.ErrNotFound
+		}
+		return p, err
+	}
+
+	return p, nil
+}
+
+func (c *ctx) Set(p kvl.Pair) error {
+	// Upsert
+	_, err := c.sqlTx.Exec(
+		"WITH "+
+			"upsert AS ("+
+			"    UPDATE data SET value = $2 WHERE key = $1 RETURNING *"+
+			") "+
+			"INSERT INTO data (key, value) SELECT $1, $2 "+
+			"    WHERE NOT EXISTS (SELECT * FROM upsert)", p.Key, p.Value)
+	if err != nil {
+		c.checkErr(err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *ctx) Delete(key []byte) error {
+	res, err := c.sqlTx.Exec("DELETE FROM data WHERE key = $1", key)
+	if err != nil {
+		c.checkErr(err)
+		return err
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		c.checkErr(err)
+		return err
+	}
+
+	if count == 0 {
+		return kvl.ErrNotFound
+	}
+
+	return nil
+}
+
+func (c *ctx) Range(q kvl.RangeQuery) ([]kvl.Pair, error) {
+	params := make([]interface{}, 0, 2)
+	query := "SELECT key, value FROM data WHERE TRUE"
+	if q.Low != nil {
+		query += fmt.Sprintf(" AND key >= $%v", len(params)+1)
+		params = append(params, q.Low)
+	}
+	if q.High != nil {
+		query += fmt.Sprintf(" AND KEY < $%v", len(params)+1)
+		params = append(params, q.High)
+	}
+	if q.Descending {
+		query += " ORDER BY key DESC"
+	} else {
+		query += " ORDER BY key ASC"
+	}
+	if q.Limit > 0 {
+		query += fmt.Sprintf(" LIMIT %v", q.Limit)
+	}
+
+	rows, err := c.sqlTx.Query(query, params...)
+	if err != nil {
+		c.checkErr(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	pairs := make([]kvl.Pair, 0, 16)
+	for rows.Next() {
+		var k, v []byte
+		err = rows.Scan(&k, &v)
+		if err != nil {
+			c.checkErr(err)
+			return nil, err
+		}
+
+		pairs = append(pairs, kvl.Pair{k, v}) // TODO: needs copy?
+	}
+	err = rows.Err()
+	if err != nil {
+		c.checkErr(err)
+		return nil, err
+	}
+
+	return pairs, nil
+}
---
+++ backend/psql/db.go
@@ -0,0 +1,95 @@
+package psql
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"git.encryptio.com/kvl"
+)
+
+type DB struct {
+	sqlDB *sql.DB
+}
+
+func Open(dsn string) (*DB, error) {
+	sqlDB, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		sqlDB.Close()
+		return nil, err
+	}
+
+	db := &DB{sqlDB: sqlDB}
+
+	err = db.ensureTable()
+	if err != nil {
+		sqlDB.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (db *DB) Close() {
+	db.sqlDB.Close()
+}
+
+func (db *DB) ensureTable() error {
+	_, err := db.sqlDB.Exec(
+		"CREATE TABLE IF NOT EXISTS " +
+			"data (" +
+			"    key bytea not null primary key," +
+			"    value bytea not null" +
+			") " +
+			"WITH ( OIDS=FALSE, fillfactor=90 )")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) RunTx(tx kvl.Tx) (interface{}, error) {
+	for {
+		ret, err, again := db.tryTx(tx)
+		if !again {
+			return ret, err
+		}
+	}
+}
+
+func (db *DB) tryTx(tx kvl.Tx) (interface{}, error, bool) {
+	sqlTx, err := db.sqlDB.Begin()
+	if err != nil {
+		return nil, err, false
+	}
+
+	_, err = sqlTx.Exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
+	if err != nil {
+		return nil, err, false
+	}
+
+	ctx := &ctx{sqlTx: sqlTx}
+
+	ret, err := tx(ctx)
+	if err != nil {
+		ctx.checkErr(err)
+		err2 := sqlTx.Rollback()
+		ctx.checkErr(err2)
+		// err2 is not returned; the first error is probably more important
+		return nil, err, ctx.needsRetry
+	}
+
+	err = sqlTx.Commit()
+	if err != nil {
+		ctx.checkErr(err)
+		return nil, err, ctx.needsRetry
+	}
+
+	return ret, nil, ctx.needsRetry
+}
