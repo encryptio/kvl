a96bc53088708faa8c1b23732872e1f142b0d245
Chris Kastorff <encryptio@gmail.com>
2015-09-16 10:55:50 -0700

    backend/tests: remove counters

--- backend/tests/ram_test.go
+++ backend/tests/ram_test.go
@@ -7,29 +7,21 @@ import (
 )
 
 func TestRAMShuffleShardedIncrement(t *testing.T) {
-	ram.ResetCounters()
 	s := ram.New()
 	testShuffleShardedIncrement(t, s)
-	t.Logf("Counters = %#v", ram.GetCounters())
 }
 
 func TestRAMRangeMaxRandomReplacement(t *testing.T) {
-	ram.ResetCounters()
 	s := ram.New()
 	testRangeMaxRandomReplacement(t, s)
-	t.Logf("Counters = %#v", ram.GetCounters())
 }
 
 func TestRAMConsistencyWithRAM(t *testing.T) {
-	ram.ResetCounters()
 	s := ram.New()
 	testRandomOpConsistencyWithRAM(t, s)
-	t.Logf("Counters = %#v", ram.GetCounters())
 }
 
 func TestRAMWatchBasic(t *testing.T) {
-	ram.ResetCounters()
 	s := ram.New()
 	testWatchBasic(t, s)
-	t.Logf("Counters = %#v", ram.GetCounters())
 }
